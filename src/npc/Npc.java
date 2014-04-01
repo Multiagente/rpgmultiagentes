24e9ba2cea9fbb1b7f9d2af6743555c6e76ea0a8
ArthurJahn <stutrzbecher@gmail.com>
2014-04-01 01:45:33 -0300

    Refatoração para mudança dos nomes de classes e métodos para o inglês.

---
+++ src/npc/Npc.java
@@ -0,0 +1,13 @@
+package npc;
+
+import jade.core.Agent;
+
+public abstract class Npc extends Agent{
+
+	/**
+	 * @author Arthur Jahn
+	 */
+	private static final long serialVersionUID = 508183949563528426L;
+	
+
+}
