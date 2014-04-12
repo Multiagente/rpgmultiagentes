73b754985a278962c7c5ba2edc87aa8632f3c4c2
Levino <Levino@Levino-PC>
2014-04-12 11:00:40 -0300

    new map

---
+++ src/map/Map.java
@@ -0,0 +1,54 @@
+package map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+
+	private static Map instance = null;
+	private static List<Square> arrayMap = new ArrayList<Square>();
+
+	private Map() {
+
+	}
+
+	public static List<Square> getArrayMap() {
+
+		return arrayMap;
+	}
+
+	public static Map getMap() {
+
+		if (instance == null) {
+			/*
+			 * At this point we can setup our default map it will include the
+			 * squares (the number is arbitrary) and all the characteristics of
+			 * the square, items, description etc. This is being done by the
+			 * method createDefaultMap().
+			 */
+			instance = new Map();
+			createDefaultMap();
+		}
+
+		return instance;
+	}
+
+	public static void addSquare(Square square) {
+		Map.getArrayMap().add(square);
+	}
+
+	private static void createDefaultMap() {
+		Square firstRoom = new Square(instance, "1- The Start Place",
+				"This is where our hero starts, the central park.");
+		Square secondRoom = new Square(instance, "2- Starting the adventure",
+				"You are now at a store.");
+		Square thirdRoom = new Square(instance, "3- Third place you can go.",
+				"You are at the edge of the world.");
+
+		firstRoom.setSouth(secondRoom);
+		secondRoom.setNorth(firstRoom);
+		secondRoom.setSouth(thirdRoom);
+		thirdRoom.setNorth(secondRoom);
+	}
+
+}
---
+++ src/map/Square.java
@@ -0,0 +1,90 @@
+package map;
+
+import item.Item;
+import java.util.ArrayList;
+import java.util.List;
+import npc.Npc;
+
+public class Square {
+
+	private String title;
+	private String description;
+	private List<Item> items = new ArrayList<Item>();
+	private List<Npc> npcs = new ArrayList<Npc>();
+	private Square north, south, east, west;
+	
+	public Square() {
+	
+	}
+
+	public Square(Map map, String title, String description) {
+
+		this.title = title;
+		this.description = description;
+		Map.addSquare(this);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Square getNorth() {
+		return north;
+	}
+
+	public void setNorth(Square north) {
+		this.north = north;
+	}
+
+	public Square getSouth() {
+		return south;
+	}
+
+	public void setSouth(Square south) {
+		this.south = south;
+	}
+
+	public Square getEast() {
+		return east;
+	}
+
+	public void setEast(Square east) {
+		this.east = east;
+	}
+
+	public Square getWest() {
+		return west;
+	}
+
+	public void setWest(Square west) {
+		this.west = west;
+	}
+
+	public void addNpc(Npc npc) {
+		npcs.add(npc);
+	}
+
+	public void removeNpc(Npc npc) {
+		npcs.remove(npc);
+	}
+
+	public void addItem(Item item) {
+		items.add(item);
+	}
+
+	public void removeItem(Item item) {
+		items.remove(item);
+	}
+}
