7c798628d6185b7f8f5d429f50a4d13023114c39
Thaiane <thaiane.unb@hotmail.com>
2014-04-20 19:53:10 -0300

    Implementing model class Item

--- src/item/Item.java
+++ src/item/Item.java
@@ -1,35 +1,40 @@
 package item;
 
-import jade.core.Agent;
-import jade.domain.DFService;
-import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
+/* This class represents the basic and essential attributes of any item in the world of 
+ * adventure game. Through this model can form more specialized items, like weapons and armor. 
+ * So it is an abstract class. It only intention is to model items does not exist concretely. */
+public abstract class Item {
 
-public abstract class Item extends Agent{
+	private String name = "";
+	private double weight = 0;
+	private double cost = 0;
+
+	public Item(String denomination, double totalWeight) {	
+		this.setName(denomination);
+		this.setWeight(totalWeight);
+	}
+
+	public String getName() {
+		return name;
+	}
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1237566209096802683L;
+	public double getWeight() {
+		return weight;
+	}
 	
-	public void setUp(){
-		DFAgentDescription dfd = new DFAgentDescription();
-		dfd.setName(getAID());
-		ServiceDescription sd = new ServiceDescription();
-		sd.setType("Adoracao");
-		sd.setName("Adorar");
-		dfd.addServices(sd);
+	public double getCost() {
+		return cost;
+	}
+	
+	public void setCost(double value) {
+		this.cost = value;
+	}
 
-		try {
-			DFService.register(this, dfd);
+	private void setName(String denomination) {
+		this.name = denomination;
+	}
 
-		} catch (FIPAException fe) {
-			fe.printStackTrace();
-
-		}
+	private void setWeight(double totalWeight) {
+		this.weight = totalWeight;
 	}
 }
-	
-
-
