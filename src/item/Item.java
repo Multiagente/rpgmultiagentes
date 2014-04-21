97e6a6f4ea5a0e41a2ec7c0e0a2dc2071d25f6da
TomazMartins <tomaz.unb@outlook.com>
2014-04-20 21:48:01 -0300

    Small changes in the code of the enumeration in class Item

--- src/item/Item.java
+++ src/item/Item.java
@@ -5,11 +5,27 @@ package item;
  * So it is an abstract class. It only intention is to model items does not exist concretely. */
 public abstract class Item {
 	
-	public enum classificationOfItemRarity {
-		COMMON, UNCOMMON, RARE, UNIQUE
+	/* This enumeration helps organize the types of items in relation 
+	 * to its rarity in the game world. */
+	public enum ClassificationOfItemRarity {
+		COMMON("Common"),
+		UNCOMMON("Uncommon"),
+		RARE("Rare"),
+		UNIQUE("Unique");
+		
+		private final String rarityValue;
+		
+		ClassificationOfItemRarity(String rarity) {
+			this.rarityValue = rarity;
+		}
+		
+		/* Case for viewing in "words" type of rarity of the item is necessary */
+		public String getRarityValue() {
+			return this.rarityValue;
+		}
 	}
 	
-	private int itemRarity = 0;
+	private ClassificationOfItemRarity itemRarity = ClassificationOfItemRarity.COMMON;
 	private String itemName = "";
 	private double itemWeight = 0;	// The weight is measured in kilograms
 	private double itemCost = 0;	// The cost is measured in Gold Pieces
@@ -19,7 +35,7 @@ public abstract class Item {
 		this.setWeight(totalWeight);
 	}
 	
-	public int getItemRarity() {
+	public ClassificationOfItemRarity getItemRarity() {
 		return itemRarity;
 	}
 
@@ -35,7 +51,7 @@ public abstract class Item {
 		return itemCost;
 	}
 	
-	public void setItemRarity(int itemRarity) {
+	public void setItemRarity(ClassificationOfItemRarity itemRarity) {
 		this.itemRarity = itemRarity;
 	}
 	
