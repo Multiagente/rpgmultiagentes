0bc707fd28b3cec826b01e9c2becafaa85eaad7a
Thaiane <thaiane.unb@hotmail.com>
2014-04-22 00:38:37 -0300

    Replacing the AdventureEquipament class by class SmallStone, a simple item

---
+++ src/item/SmallStone.java
@@ -0,0 +1,29 @@
+package item;
+
+/* This class is a specialization of the abstract class Item. Represents the most mundane items 
+ * in the game world. Are items such as backpacks, torches, clothing, ropes, tents and food.*/
+public class SmallStone extends Item {
+	
+	private static final String NAME_OF_STONE = "Small Stone";
+	private static final double STONE_WEIGHT = 0.250;
+	
+	private SmallStone instanceOfSmallStone = null;
+	
+	public SmallStone(String denomination, double totalWeight) {
+		super(denomination, totalWeight);
+		
+		this.setItemRarity(ClassificationOfItemRarity.COMMON);
+	}
+	
+	public SmallStone getInstance() {
+		
+		if(instanceOfSmallStone != null) {
+			// Nothing to do
+			
+		} else {
+			instanceOfSmallStone = new SmallStone(NAME_OF_STONE, STONE_WEIGHT);
+		}
+		
+		return instanceOfSmallStone;
+	}
+}
