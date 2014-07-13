d5fbf7dbb4c4304b20dd94ee86b08d996c8ad886
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-12 21:00:08 -0300

    Adequando CharacterDao e DBConnector ao padrão

--- src/br/unb/sma/dd/infrastructure/dao/CharacterDao.java
+++ src/br/unb/sma/dd/infrastructure/dao/CharacterDao.java
@@ -1,4 +1,8 @@
+/* File:		TODO: put name of the file.
+ * Purpose: TODO: put a text about the purpose of the file.
+ * */
 package br.unb.sma.dd.infrastructure.dao;
+
 
 import br.unb.sma.dd.infrastructure.persistenceHelper.DBConnector;
 import br.unb.sma.dd.world.character.Character;
@@ -7,30 +11,34 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.CommandResult;
 import com.mongodb.DBCollection;
 
-public class CharacterDao {
 
+/** TODO: put a text about the this class. */
+public class CharacterDao {
+	
 	private DBConnector dbConnector;
 	private static CharacterDao instance = null;
 	
-	private CharacterDao(){
+	private CharacterDao() {
 		dbConnector = DBConnector.getInstance();
 	}
 	
-	public CharacterDao getInstance(){
-		if(instance == null){
+	public CharacterDao getInstance() {
+		if( instance == null ) {
 			instance = new CharacterDao();
-		}
+			
+		} // TODO: put the default behavior.
+		
 		return instance;
 	}
 	
-	public boolean insertCharacter (Character character){
-		DBCollection table = dbConnector.getTable("characterTable");
+	/** TODO: put a text about this method. */
+	public boolean insertCharacter( Character character ) {
+		DBCollection table = dbConnector.getTable( "characterTable" );
 		BasicDBObject document = new BasicDBObject();
-//Put Character data here like:  		
-//		document.put("name", character.getName());
-//		document.put("strength", character.getStrength());
-		CommandResult result = table.insert(document).getLastError(); 
+		// Put Character data here like:
+		// document.put("name", character.getName());
+		// document.put("strength", character.getStrength());
+		CommandResult result = table.insert( document ).getLastError();
 		return result.ok();
 	}
-	
 }
--- src/br/unb/sma/dd/infrastructure/persistenceHelper/DBConnector.java
+++ src/br/unb/sma/dd/infrastructure/persistenceHelper/DBConnector.java
@@ -1,4 +1,8 @@
+/* File:		TODO: put name of the file.
+ * Purpose: TODO: put a text about the purpose of the file.
+ * */
 package br.unb.sma.dd.infrastructure.persistenceHelper;
+
 
 import java.net.UnknownHostException;
 
@@ -6,30 +10,35 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
-public class DBConnector {
 
-	private static DBConnector instance = null;
+/** TODO: put a text about the this class. */
+public class DBConnector {
 	
+	private static DBConnector instance = null;
 	private MongoClient mongo;
 	
-	private DBConnector(){
+	private DBConnector() {
 		try {
-			mongo = new MongoClient("localhost", 27017);
-		} catch (UnknownHostException e) {
+			mongo = new MongoClient( "localhost", 27017 );
+			
+		} catch( UnknownHostException e ) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static DBConnector getInstance() {
-		if(instance == null){
+		if( instance == null ) {
 			instance = new DBConnector();
-		}
+			
+		} // TODO: put the default behavior.
+		
 		return instance;
 	}
-	
-	public DBCollection getTable(String tableName){
-		DB database = mongo.getDB("rpgMultiagentesDb");
-		DBCollection table = database.getCollection(tableName);
+
+	/** TODO: put a text about this method. */
+	public DBCollection getTable( String tableName ) {
+		DB database = mongo.getDB( "rpgMultiagentesDb" );
+		DBCollection table = database.getCollection( tableName );
 		return table;
 	}
 }
