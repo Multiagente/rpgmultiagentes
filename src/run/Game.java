efcbc499db621db0f02cad4fc85a486aa9b353f7
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-18 16:14:07 -0300

    Corrigindo nomes de variáveis e importação de pacotes

--- src/run/Game.java
+++ src/run/Game.java
@@ -6,7 +6,6 @@ import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.StaleProxyException;
-import jade.tools.*;
 
 import java.util.Iterator;
 
@@ -16,30 +15,43 @@ import br.unb.sma.dd.world.map.Square;
 public class Game {
 	public static void main(String[] args) {
 		Map map = Map.getMap();	
-		map.initializeMap();
-		Iterator<Square> i = map.getArrayMap().iterator();
 		
-		while (i.hasNext()) {
-			System.out.println(i.next().getTitle());
+		map.initializeMap();
+		
+		Iterator<Square> iteratorSquares = map.getArrayMap().iterator();
+		
+		while (iteratorSquares.hasNext()) {
+			System.out.println(iteratorSquares.next().getTitle());
 		}
-		Runtime rt = Runtime.instance();
-		Profile p = new ProfileImpl(null, 31000, null);
-		AgentContainer cont = rt.createMainContainer(p);
+		
+		Runtime gameRunTime = Runtime.instance();
+		Profile profile = new ProfileImpl(null, 31000, null);
+		AgentContainer mainContainer = gameRunTime.createMainContainer(profile);
+		
 		try {
-			AgentController rma = cont.createNewAgent("rma", 
-					"jade.tools.rma.rma", new Object[0]);
+			AgentController rma = mainContainer.createNewAgent("rma", 
+					"jade.tools.rma.rma", new Object[ 0 ]);
+			
 			rma.start();
-			AgentController character1 = cont.createNewAgent("player1", 
+			
+			AgentController character1 = mainContainer.createNewAgent("player1", 
 					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			
 			character1.start();
-			AgentController character2 = cont.createNewAgent("player2", 
+			
+			AgentController character2 = mainContainer.createNewAgent("player2", 
 					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			
 			character2.start();
-			AgentController character3 = cont.createNewAgent("player3", 
+			
+			AgentController character3 = mainContainer.createNewAgent("player3", 
 					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+
 			character3.start();
-			AgentController character4 = cont.createNewAgent("player4", 
+			
+			AgentController character4 = mainContainer.createNewAgent("player4", 
 					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			
 			character4.start();
 			
 		} catch (StaleProxyException e) {
