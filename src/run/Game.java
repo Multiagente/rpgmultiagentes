33b8b75ed0cf55e735b45eed50a827f14397ed4b
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-18 16:21:27 -0300

    Adicionando constantes para referenciar alocalização das classes CharacterAgent e rma

--- src/run/Game.java
+++ src/run/Game.java
@@ -1,5 +1,6 @@
 package run;
 
+
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
@@ -12,49 +13,55 @@ import java.util.Iterator;
 import br.unb.sma.dd.world.map.Map;
 import br.unb.sma.dd.world.map.Square;
 
+
 public class Game {
-	public static void main(String[] args) {
-		Map map = Map.getMap();	
+	private static final String CHARACTER_AGENT_CLASS = 
+			"br.unb.sma.dd.world.character.CharacterAgent";
+	
+	private static final String RMA_CLASS = "jade.tools.rma.rma";
+	
+	public static void main( String[] args ) {
+		Map map = Map.getMap();
 		
 		map.initializeMap();
 		
 		Iterator<Square> iteratorSquares = map.getArrayMap().iterator();
 		
-		while (iteratorSquares.hasNext()) {
-			System.out.println(iteratorSquares.next().getTitle());
+		while( iteratorSquares.hasNext() ) {
+			System.out.println( iteratorSquares.next().getTitle() );
 		}
 		
 		Runtime gameRunTime = Runtime.instance();
-		Profile profile = new ProfileImpl(null, 31000, null);
-		AgentContainer mainContainer = gameRunTime.createMainContainer(profile);
+		Profile profile = new ProfileImpl( null, 31000, null );
+		AgentContainer mainContainer = gameRunTime.createMainContainer( profile );
 		
 		try {
-			AgentController rma = mainContainer.createNewAgent("rma", 
-					"jade.tools.rma.rma", new Object[ 0 ]);
+			AgentController rma = mainContainer.createNewAgent( "rma",
+			      RMA_CLASS, new Object[ 0 ] );
 			
 			rma.start();
 			
-			AgentController character1 = mainContainer.createNewAgent("player1", 
-					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			AgentController character1 = mainContainer.createNewAgent( "player1",
+			      CHARACTER_AGENT_CLASS, null );
 			
 			character1.start();
 			
-			AgentController character2 = mainContainer.createNewAgent("player2", 
-					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			AgentController character2 = mainContainer.createNewAgent( "player2",
+					CHARACTER_AGENT_CLASS, null );
 			
 			character2.start();
 			
-			AgentController character3 = mainContainer.createNewAgent("player3", 
-					"br.unb.sma.dd.world.character.CharacterAgent" , null);
-
+			AgentController character3 = mainContainer.createNewAgent( "player3",
+					CHARACTER_AGENT_CLASS, null );
+			
 			character3.start();
 			
-			AgentController character4 = mainContainer.createNewAgent("player4", 
-					"br.unb.sma.dd.world.character.CharacterAgent" , null);
+			AgentController character4 = mainContainer.createNewAgent( "player4",
+					CHARACTER_AGENT_CLASS, null );
 			
 			character4.start();
 			
-		} catch (StaleProxyException e) {
+		} catch( StaleProxyException e ) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
