b594e7f60568bea0d9f7f0ebe8f087fc9a464981
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-19 19:33:24 -0300

    Alterando nomes e identação da classe CharacterAgent para adequação com o padrão

--- src/br/unb/sma/dd/world/character/CharacterAgent.java
+++ src/br/unb/sma/dd/world/character/CharacterAgent.java
@@ -1,8 +1,9 @@
-/* 
- * File:	Character.java
- * Purpose: This is a agent that represents a character in the game.
+/*
+ * File: Character.java Purpose: This is a agent that represents a character in
+ * the game.
  */
 package br.unb.sma.dd.world.character;
+
 
 import jade.core.AID;
 import jade.core.Agent;
@@ -30,42 +31,45 @@ import java.util.Vector;
 import br.unb.sma.dd.world.map.Map;
 import br.unb.sma.dd.world.map.Square;
 
-/*
- *This class is a specialization of the abstract class Agent. 
- *It holds the static and behavioral properties of the game's character. 
- */
 
+/**
+ * This class is a specialization of the abstract class Agent.It holds the
+ * static and behavioral properties of the game's character.
+ */
 public class CharacterAgent extends Agent {
 	private static final long serialVersionUID = 3380309901133173161L;
+	
 	public CharacterAgent mySelf = this;
 	public Map map = null;
-	private Square positionNow = null;
+	
+	private Square currentPosition = null;
 	private Square nextPosition = null;
+	
 	private int healthPoints = 100;
 	private int defensePoints = 2;
 	private int attackPoints = 20;
 	private int experiencePoints = 10;
-
-	public void setPosition( Square sqr ) {
-		this.positionNow = sqr;
+	
+	public void setPosition( Square currentSquare ) {
+		this.currentPosition = currentSquare;
 	}
-
+	
 	public Square getPosition() {
-		return this.positionNow;
+		return this.currentPosition;
 	}
-
+	
 	public int getHealthPoints() {
 		return this.healthPoints;
 	}
 	
-	public void setHealthPoints( int value ){
+	public void setHealthPoints( int value ) {
 		this.healthPoints = value;
 	}
 	
 	public Square getNextPosition() {
 		return nextPosition;
 	}
-
+	
 	public void setNextPosition( Square nextPosition ) {
 		this.nextPosition = nextPosition;
 	}
@@ -73,7 +77,7 @@ public class CharacterAgent extends Agent {
 	public CharacterAgent getChar() {
 		return this.mySelf;
 	}
-
+	
 	public void setup() {
 		map = Map.getMap();
 		DFAgentDescription dfd = new DFAgentDescription();
@@ -81,123 +85,149 @@ public class CharacterAgent extends Agent {
 		
 		setPosition( map.getArrayMap().get( 0 ) );
 		dfd.setName( getAID() );
+		
 		sd.setType( "MessageService" );
 		sd.setName( "Mail" );
+		
 		dfd.addServices( sd );
+		
 		getPosition().addAgent( this.getAID() );
+		
 		try {
 			DFService.register( this, dfd );
-
+			
 		} catch( FIPAException fe ) {
 			fe.printStackTrace();
 		}
-		//addBehaviour(new Move(this,500));
-		//addBehaviour(new HandleMoveRequest());
+		// addBehaviour(new Move(this,500));
+		// addBehaviour(new HandleMoveRequest());
 		addBehaviour( new LookForFight( this, 2000 ) );
 		addBehaviour( new HandleFightRequest() );
 	}
-
+	
 	protected void takeDown() {
 		try {
 			DFService.deregister( this );
-		} catch ( FIPAException fe ) {
+			
+		} catch( FIPAException fe ) {
 			fe.printStackTrace();
+			
 		}
 	}
-		
+	
 	private void exitRoom() {
-		ACLMessage msg = new ACLMessage( ACLMessage.REQUEST );
-		List<AID> listChars = getPosition().charactersInside();
+		ACLMessage message = new ACLMessage( ACLMessage.REQUEST );
+		List<AID> listCharacters = getPosition().charactersInside();
 		
-		if( listChars.size() > 1 ){
-			Iterator<AID> i = listChars.iterator();
-			while( i.hasNext() ){
-				AID receiver = i.next();
-				if( this.getAID()!=receiver )
-					msg.addReceiver( receiver );
+		if( listCharacters.size() > 1 ) {
+			Iterator<AID> iterator = listCharacters.iterator();
+			
+			while( iterator.hasNext() ) {
+				AID receiver = iterator.next();
+				
+				if( this.getAID() != receiver ) {
+					message.addReceiver( receiver );
+					
+				} else {
+					/* ! Nothing To Do. */
+					
+				}
 			}
 		}
-		msg.setProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST );
-		msg.setReplyByDate( new Date( System.currentTimeMillis() + 1000 ) );
-		msg.setContent( "leave" );
-		this.send( msg );
-		addBehaviour( new AchieveREInitiator( this, msg ) {
+		
+		message.setProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST );
+		message.setReplyByDate( new Date( System.currentTimeMillis() + 1000 ) );
+		message.setContent( "leave" );
+		
+		this.send( message );
+		
+		addBehaviour( new AchieveREInitiator( this, message ) {
 			private static final long serialVersionUID = 1202650982398071473L;
 			boolean canLeave = true;
 			
 			protected void handleInform( ACLMessage inform ) {
-				System.out.println( "Agent " + inform.getSender().getName() + 
-						" opose your left." );
+				System.out.println( "Agent " + inform.getSender().getName()
+				      + " opose your left." );
+				
 				canLeave = false;
 			}
-
+			
 			protected void handleRefuse( ACLMessage refuse ) {
-				System.out.println( "Agent " + refuse.getSender().getName() + 
-						" refused " + myAgent.getName() + " msg." );
+				System.out.println( "Agent " + refuse.getSender().getName()
+				      + " refused " + myAgent.getName() + " msg." );
 			}
-
+			
 			protected void handleFailure( ACLMessage failure ) {
-				if ( failure.getSender().equals( myAgent.getAMS() )) {
+				if( failure.getSender().equals( myAgent.getAMS() ) ) {
 					System.out.println( "This guy might be dead." );
 				} else {
 					System.out.println( "Agent " + failure.getSender().getName()
-							+ " failed to perform the requested action" );
+					      + " failed to perform the requested action" );
 				}
 			}
-
+			
 			protected void handleAllResultNotifications( Vector notifications ) {
 				System.out.println( "Timeout expired: missing "
-						+ ( notifications.size() ) + " responses" );
-				if( canLeave ){
+				      + ( notifications.size() ) + " responses" );
+				if( canLeave ) {
 					try {
 						updatePosition( nextMove() );
-					} catch ( FIPAException e ) {
+					} catch( FIPAException e ) {
 						e.printStackTrace();
 					}
 				}
 			}
-		});
+		} );
 	}
-
+	
 	private void fight( AID target ) {
 		ACLMessage msg = new ACLMessage( ACLMessage.REQUEST );
 		AID receiver = target;
 		
-		if( this.getAID()!=receiver )
+		if( this.getAID() != receiver ) {
 			msg.addReceiver( receiver );
+			
+		} else {
+			/*! Nothing To Do. */
+			
+		}
+		
 		msg.setProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST );
 		msg.setReplyByDate( new Date( System.currentTimeMillis() + 1000 ) );
 		msg.setContent( String.valueOf( getAttackPoints() ) );
+		
 		this.send( msg );
 		
-		addBehaviour(new AchieveREInitiator( this, msg ) {
+		addBehaviour( new AchieveREInitiator( this, msg ) {
 			private static final long serialVersionUID = 1202650982398071473L;
 			
 			protected void handleInform( ACLMessage inform ) {
-				/* Receive experience points in the end of each attack.*/
+				/* Receive experience points in the end of each attack. */
 				
-				int exp =( int ) ( Integer.parseInt( inform.getContent() ) / getExperiencePoints() );
-				System.out.println( "["+getLocalName()+"]: got "+ exp +
-						" experience points from [" + inform.getSender().getLocalName() + "]" );
+				int exp = (int) ( Integer.parseInt( inform.getContent() ) / getExperiencePoints() );
+				System.out.println( "[" + getLocalName() + "]: got " + exp
+				      + " experience points from ["
+				      + inform.getSender().getLocalName() + "]" );
 				updateExperiencePoints( exp );
 			}
-
+			
 			protected void handleAllResultNotifications( Vector notifications ) {
-				//System.out.println("[FIGHT:"+getLocalName()+"] Timeout expired: missing "	+ (notifications.size()) + " responses");
+				// System.out.println("[FIGHT:"+getLocalName()+"] Timeout expired: missing "
+				// + (notifications.size()) + " responses");
 			}
-		});
+		} );
 	}
 	
 	private int getAttackPoints() {
 		return this.attackPoints;
 	}
-
+	
 	private int getExperiencePoints() {
 		return this.experiencePoints;
 	}
-
+	
 	private void updateExperiencePoints( int value ) {
-		this.experiencePoints += value;		
+		this.experiencePoints += value;
 	}
 	
 	private int getDefensePoints() {
@@ -208,66 +238,67 @@ public class CharacterAgent extends Agent {
 		this.defensePoints = value;
 	}
 	
-	private void updatePosition( Square newPos ) throws FIPAException {
+	private void updatePosition( Square newPosition ) throws FIPAException {
 		Square sqr = getPosition();
+		
 		sqr.removeCharacter( this.getAID() );
-		setPosition( newPos );
+		setPosition( newPosition );
 		sqr = getPosition();
 		sqr.addAgent( this.getAID() );
-		System.out.println( newPos.getDescription() );
+		
+		System.out.println( newPosition.getDescription() );
 	}
 	
 	private void damageHealthPoints( int value ) {
-		if( getHealthPoints() < 0 ){
+		if( getHealthPoints() < 0 ) {
 			setHealthPoints( 0 );
 			getPosition().removeCharacter( this.getAID() );
 			this.doDelete();
-		}
-		else
-			setHealthPoints( getHealthPoints() - ( value-defensePoints ) );
+		} else
+			setHealthPoints( getHealthPoints() - ( value - defensePoints ) );
 	}
-
+	
 	private Square nextMove() {
 		Square sqr = getPosition();
 		List<Square> possibleMoves = new ArrayList<Square>();
-
-		if (sqr.getNorth() != null){
+		
+		if( sqr.getNorth() != null ) {
 			possibleMoves.add( sqr.getNorth() );
 		}
-		if (sqr.getSouth() != null){
+		if( sqr.getSouth() != null ) {
 			possibleMoves.add( sqr.getSouth() );
 		}
-		if (sqr.getEast() != null){
+		if( sqr.getEast() != null ) {
 			possibleMoves.add( sqr.getEast() );
 		}
-		if (sqr.getWest() != null){
+		if( sqr.getWest() != null ) {
 			possibleMoves.add( sqr.getWest() );
 		}
-		Square newPos = possibleMoves.get( ( (int) Math.random()*100)
-				% possibleMoves.size() );
+		Square newPos = possibleMoves.get( ( (int) Math.random() * 100 )
+		      % possibleMoves.size() );
 		return newPos;
 	}
-
-	private class Move extends TickerBehaviour{
+	
+	private class Move extends TickerBehaviour {
 		private static final long serialVersionUID = -679586132985982801L;
-
+		
 		public Move( Agent a, long period ) {
 			super( a, period );
 		}
-
+		
 		@Override
 		protected void onTick() {
 			exitRoom();
-		}	
+		}
 	}
-
-	private class LookForFight extends TickerBehaviour{
+	
+	private class LookForFight extends TickerBehaviour {
 		private static final long serialVersionUID = 4461404912449987804L;
 		
 		public LookForFight( Agent a, long period ) {
 			super( a, period );
 		}
-
+		
 		@Override
 		protected void onTick() {
 			AID id;
@@ -277,105 +308,110 @@ public class CharacterAgent extends Agent {
 		}
 	}
 	
-	private class HandleMoveRequest extends OneShotBehaviour{
+	private class HandleMoveRequest extends OneShotBehaviour {
 		private static final long serialVersionUID = 6230563887514017233L;
 		
 		@Override
 		public void action() {
-			MessageTemplate template = MessageTemplate.and(
-			  		MessageTemplate.MatchProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST ),
-			  		MessageTemplate.MatchPerformative( ACLMessage.REQUEST ) );
+			MessageTemplate template = MessageTemplate.and( MessageTemplate
+			      .MatchProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST ),
+			      MessageTemplate.MatchPerformative( ACLMessage.REQUEST ) );
 			mySelf.addBehaviour( new AchieveREResponder( mySelf, template ) {
 				private static final long serialVersionUID = 1L;
-
-				protected ACLMessage prepareResponse( ACLMessage request ) 
-						throws NotUnderstoodException, RefuseException {
-					System.out.println( "Agent " + getLocalName() + ": REQUEST received from " +
-					request.getSender().getName() + ". Action is " + request.getContent() );
-					if ( checkAction() ) {
+				
+				protected ACLMessage prepareResponse( ACLMessage request )
+				      throws NotUnderstoodException, RefuseException {
+					System.out.println( "Agent " + getLocalName()
+					      + ": REQUEST received from "
+					      + request.getSender().getName() + ". Action is "
+					      + request.getContent() );
+					if( checkAction() ) {
 						/*
-						 * We agree to perform the action. Note that in the FIPA-Request
-						 * protocol the AGREE message is optional. Return null if you
-					 	 * don't want to send it.
-						*/
+						 * We agree to perform the action. Note that in the
+						 * FIPA-Request protocol the AGREE message is optional. Return
+						 * null if you don't want to send it.
+						 */
 						
 						System.out.println( "Agent " + getLocalName() + ": Agree" );
 						ACLMessage agree = request.createReply();
 						agree.setPerformative( ACLMessage.AGREE );
 						return agree;
-					}
-					else {
+					} else {
 						// We refuse to perform the action
 						System.out.println( "Agent " + getLocalName() + ": Refuse" );
 						throw new RefuseException( "check-failed" );
 					}
 				}
 				
-				protected ACLMessage prepareResultNotification( ACLMessage request, 
-						ACLMessage response ) throws FailureException {
-					if ( performAction() ) {
-						System.out.println( "Agent " + getLocalName() + ": Action successfully performed" );
+				protected ACLMessage prepareResultNotification( ACLMessage request,
+				      ACLMessage response ) throws FailureException {
+					if( performAction() ) {
+						System.out.println( "Agent " + getLocalName()
+						      + ": Action successfully performed" );
 						ACLMessage inform = request.createReply();
 						inform.setPerformative( ACLMessage.INFORM );
 						return inform;
-					}
-					else {
-						System.out.println( "Agent " + getLocalName() + ": Action failed" );
+					} else {
+						System.out.println( "Agent " + getLocalName()
+						      + ": Action failed" );
 						throw new FailureException( "unexpected-error" );
-					}	
+					}
 				}
 			} );
 		}
-
+		
 		private boolean checkAction() {
-		   /* Adicionar verificação para as possíveis ações dos agentes, casos estejam ocupados.
-			*  Simulate a check by generating a random number.
-			*/
+			/*
+			 * Adicionar verificação para as possíveis ações dos agentes,
+			 * casos estejam ocupados. Simulate a check by generating a random
+			 * number.
+			 */
 			
 			return ( Math.random() > 0.2 );
 		}
-
+		
 		private boolean performAction() {
-		  	/* Simulate action execution by generating a random number.*/
+			/* Simulate action execution by generating a random number. */
 			
-		  	return ( Math.random() > 0.2 );
+			return ( Math.random() > 0.2 );
 		}
 	}
 	
-	private class HandleFightRequest extends OneShotBehaviour{
+	private class HandleFightRequest extends OneShotBehaviour {
 		private static final long serialVersionUID = 6230563887514017233L;
 		
 		@Override
 		public void action() {
-			MessageTemplate template = MessageTemplate.and(
-			  		MessageTemplate.MatchProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST ),
-			  		MessageTemplate.MatchPerformative( ACLMessage.REQUEST ) );
-			  		
-					mySelf.addBehaviour( new AchieveREResponder( mySelf, template ) {
-						private static final long serialVersionUID = 1L;
-
-						protected ACLMessage prepareResponse( ACLMessage request ) 
-								throws NotUnderstoodException, RefuseException {
-							return null;
-						}
-
-						protected ACLMessage prepareResultNotification( ACLMessage request, 
-								ACLMessage response ) throws FailureException {
-							/* Quanto de dano esse agente perde.*/
-							
-							int atk = Integer.parseInt( request.getContent() );
-							damageHealthPoints( atk );
-							System.out.println( "[" + getLocalName() +
-									"]{HP:" + getHealthPoints() + "} Received a hit from ["
-									+ request.getSender().getLocalName() + "]{HIT:" + request.getContent() + "}" );
-							ACLMessage inform = request.createReply();
-							inform.setPerformative( ACLMessage.INFORM );
-							int exp = getHealthPoints() - ( atk - getDefensePoints() );
-							inform.setContent( String.valueOf( exp ) );
-							return inform;
-						}
-					} );
-			  }
+			MessageTemplate template = MessageTemplate.and( MessageTemplate
+			      .MatchProtocol( FIPANames.InteractionProtocol.FIPA_REQUEST ),
+			      MessageTemplate.MatchPerformative( ACLMessage.REQUEST ) );
+			
+			mySelf.addBehaviour( new AchieveREResponder( mySelf, template ) {
+				private static final long serialVersionUID = 1L;
+				
+				protected ACLMessage prepareResponse( ACLMessage request )
+				      throws NotUnderstoodException, RefuseException {
+					return null;
+				}
+				
+				protected ACLMessage prepareResultNotification( ACLMessage request,
+				      ACLMessage response ) throws FailureException {
+					/* Quanto de dano esse agente perde. */
+					
+					int atk = Integer.parseInt( request.getContent() );
+					damageHealthPoints( atk );
+					System.out.println( "[" + getLocalName() + "]{HP:"
+					      + getHealthPoints() + "} Received a hit from ["
+					      + request.getSender().getLocalName() + "]{HIT:"
+					      + request.getContent() + "}" );
+					ACLMessage inform = request.createReply();
+					inform.setPerformative( ACLMessage.INFORM );
+					int exp = getHealthPoints() - ( atk - getDefensePoints() );
+					inform.setContent( String.valueOf( exp ) );
+					return inform;
+				}
+			} );
+		}
 	}
-
+	
 }
