8d6940272ab42a491914f7418124928051b09299
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-19 19:43:08 -0300

    Alterando a classe FactoryNPC para adequar-se ao padrão de estilo do projeto.

--- src/br/unb/sma/dd/world/npc/FactoryNPC.java
+++ src/br/unb/sma/dd/world/npc/FactoryNPC.java
@@ -1,17 +1,31 @@
+/*
+ * File: FactoryNPC.java Purpose: Brings the implementation of a design pattern,
+ * a factory to build the type NPC agents.
+ */
 package br.unb.sma.dd.world.npc;
 
+
+/**
+ * This class is a factory as the default project, which aims to build the type
+ * NPC agents.
+ */
 public class FactoryNPC {
 	
 	public static FactoryNPC factory;
 	
-	
 	protected FactoryNPC() {
 	}
 	
-	public static FactoryNPC getFactory(){
+	public static FactoryNPC getFactory() {
 		
-		if (factory == null) 
+		if( factory == null ) {
 			factory = new FactoryNPC();
-			return factory;
-	}	
+			
+		} else {
+			/* ! Nothing To Do. */
+			
+		}
+		
+		return factory;
+	}
 }
