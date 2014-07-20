e61d091b30b5385618f5195dc53054e5044cf685
Tomaz Martins <tomaz.unb@outlook.com>
2014-07-19 23:49:28 -0300

    Alterando a classe NpcAgent para adequar-se ao padrão de estilo do projeto

--- src/br/unb/sma/dd/world/npc/NpcAgent.java
+++ src/br/unb/sma/dd/world/npc/NpcAgent.java
@@ -1,3 +1,8 @@
+/*
+ * File:		NpcAgent.java
+ * Purpose: Brings the implementation of agent that represents a generalization 
+ * of non-player characters in the world.
+ */
 package br.unb.sma.dd.world.npc;
 
 
@@ -10,6 +15,9 @@ import br.unb.sma.dd.world.map.Map;
 import br.unb.sma.dd.world.map.Square;
 
 
+/**
+ * This class is a generalization of non-player characters.
+ */
 public abstract class NpcAgent extends Agent {
 	
 	private static final long serialVersionUID = 50;
@@ -26,20 +34,22 @@ public abstract class NpcAgent extends Agent {
 		this.actualPosition = actualPosition;
 	}
 	
-	public void create() {
-		
-		List<Square> options = getPositions( map.getArrayMap() );
-		Random generator = new Random();
-		int position = generator.nextInt( options.size() );
-		// TODO: organizar a inserção de npcs separadamente dos characters
-		options.get( position ).addAgent( getAID() );
-		System.out.println( "npc created at "
-		      + options.get( position ).getTitle() );
-	}
-	
 	public abstract List<Square> getPositions( List<Square> arrayMap );
 	
-	/** Start agent */
+	public void create() {
+		
+		List<Square> optionsSquare = getPositions( map.getArrayMap() );
+		Random randomGenerator = new Random();
+		int selectedPosition = randomGenerator.nextInt( optionsSquare.size() );
+		
+		/* TODO: Organize the insertion of NPCs separately from characters. */
+		
+		optionsSquare.get( selectedPosition ).addAgent( getAID() );
+		
+		System.out.println( "npc created at "
+		      + optionsSquare.get( selectedPosition ).getTitle() );
+	}
+	
 	protected void setup() {
 		addBehaviour( new TreatmentMessageBehaviour() );
 		
